f0a14956e820f2654baad47fc7ea61ffb054664e
Mahmoud Bakr <mbakr6821@gmail.com>
2021-06-16 14:50:20 +0200

    fix overflow issue

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const bars = document.querySelector('.fa-bars')
 const navList = document.querySelector('.nav-list')
-// setTimeout(() => {
-//     bars.classList.remove('none');
-// }, 10000);
+
+setTimeout(() => {
+    bars.classList.remove('none');
+}, 10000);
 
 bars.addEventListener('click', () => {
     if(navList.classList.contains('none')) {
         navList.classList.remove('none')
         navList.classList.add('unactive')
         navList.classList.remove('active')
+        navList.style.display = 'flex';
     } else {
         navList.classList.add('none')
         navList.classList.remove('unactive')
         navList.classList.add('active')
+        setTimeout(() => {
+            navList.style.display = 'none';
+        }, 1000)
     }
 })
